c85c22468fb98b2f53f277e45e7313484b8dca0c
Dan Brewster <dnbrwstr@gmail.com>
2016-04-13 12:33:48 -0400

    Additional annotations on colorchat example

--- Examples/Chat/color-chat/main.js
+++ Examples/Chat/color-chat/main.js
@@ -12,6 +12,9 @@ $('.submit-button').click(function (e) {
   $('.color-input').removeClass('is-hidden');
 });
 
+// Actually handle user input on our chat options
+// This is where we're tying particular values to 
+// colors in messages we send.
 $('.blue').click(function () {
   sendMessage(0);
 });
@@ -59,6 +62,8 @@ function sendMessage(value) {
   });
 }
 
+// This map of color values makes it easy
+// for us to interpret the messages we receive
 var colorValues = {
   0: 'blue',
   1: 'red',
@@ -68,22 +73,40 @@ var colorValues = {
   5: 'turquoise',
   6: 'green',
   7: 'navy',
-  8: 'fuchsia'
+  8: 'fuchsia',
+  9: 'tan',
+  10: 'ochre',
+  11: 'beige',
+  12: 'pink'
 };
 
+// And here's where we actually handle incoming messages
+// For each message we get, we create a new element, label
+// it with the message sender's name, and assign classes
+// based on its sender and value
 Chat.onMessage(function (data) {
+  // Create the message element
   var message = $('<div></div>');
-  $(message).addClass('message')
+  $(message).addClass('message');
+
+  // Here's where we assign the color class. We use the
+  // message's value to pull a color from our map of 
+  // colors above
   $(message).addClass(colorValues[data.value]);
 
+  // If the message is from the current user, give it a
+  // special class
   if (data.sender == username) {
     $(message).addClass('from-current-user');
   }
 
+  // Create the label element and set its html
   var messageLabel = $('<div></div>');
   $(messageLabel).html(data.sender);
 
+  // Append label to the message
   $(message).append(messageLabel);
 
+  // Append message to the container on the page
   $('.message-container').append(message);
 });
